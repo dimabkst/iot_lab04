08c398783bbd30a4baf6d8f5a309cddb36bc6ffe
dimabkst <dimabkst@gmail.com>
2024-03-25 21:09:28 +0200

    routes validation

--- Server.py
+++ Server.py
@@ -1,8 +1,80 @@
-from typing import List, Sequence
-from flask import Flask, request
+from functools import wraps
+from rfc3339_validator import validate_rfc3339
+from typing import Callable, List, Sequence
+from flask import Flask, jsonify, request
+from jsonschema import validate, ValidationError
 
 temperatures = []
 app = Flask(__name__)
+
+
+class HttpError(Exception):
+    def __init__(self, error_code: int = 500, message: str = "Something went wrong"):
+        self.error_code = error_code
+
+        self.message = message
+
+        super().__init__(self.message)
+
+
+temperatureDataSchema = {
+    'type': 'object',
+    'properties': {
+            'temperature': {'type': 'number'},
+            'time': {'type': 'string', 'format': 'date-time', "format_checker": validate_rfc3339}
+    },
+    'required': ['temperature', 'time']
+}
+
+temperatureDataBatchSchema = {
+    'type': 'object',
+    'properties': {
+        'temperatures_batch': {
+            'type': 'array',
+            'items': temperatureDataSchema
+        }
+    },
+    'required': ['temperatures_batch']
+}
+
+
+def validate_json(data, schema):
+    try:
+        validate(data, schema)
+
+        return True, None
+    except ValidationError as e:
+        return False, e.message
+
+
+def validate_route(json_schema):
+    if json_schema:
+        request_data = request.json
+
+        is_valid, error_message = validate_json(request_data, json_schema)
+
+        if not is_valid:
+            raise HttpError(422, error_message or 'Validation error')
+
+
+def route_wrapper(schema=None):
+    def decorator(handler: Callable):
+        @wraps(handler)
+        def wrapper(*args, **kwargs):
+            try:
+                validate_route(schema)
+
+                return handler(*args, **kwargs)
+            except Exception as e:
+                http_error = e if isinstance(e, HttpError) else HttpError()
+
+                message, error_code = http_error.message, http_error.error_code
+
+                print(f'Error: {e}. Code: {error_code}. Message: {message}')
+
+                return jsonify({"error": message}), error_code
+        return wrapper
+    return decorator
 
 
 def get_temperature_values() -> List[float]:
@@ -20,15 +92,15 @@ def get_temperatures_variance(data: Sequence[float]) -> float:
 
 
 @app.route("/", methods=["GET"])
+@route_wrapper()
 def get_temperatures():
     return temperatures
 
 
 @app.route("/", methods=["POST"])
+@route_wrapper(temperatureDataSchema)
 def add_temperature():
     body = request.json
-
-    print(body)
 
     if body:
         temperatures.append(body["temperature_data"])
@@ -37,6 +109,7 @@ def add_temperature():
 
 
 @app.route("/batch", methods=["POST"])
+@route_wrapper(temperatureDataBatchSchema)
 def add_temperatures():
     body = request.json
 
